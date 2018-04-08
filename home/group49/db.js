c2db9930bad1f708ad00de7243b5dbe0debf9e50
Vogel100 <fabbimaggi@hotmail.com>
2018-04-08 22:18:08 +0200

    Made it a bit prettier

--- home/group49/db.js
+++ home/group49/db.js
@@ -77,69 +77,72 @@ class DBItem {
         db.run(`UPDATE ${this.table} SET ${placeholders} WHERE ${this.idName} = ?;`, this.propValues.concat([this.id]));
     }
 }
-//Represents a user, or a row in the Users table.
-module.exports.User = class extends DBItem {
-    constructor(id, params) {
-        super(id, params, ['login', 'password', 'first_name', 'last_name', 'email']);
-        this.table = 'Users'
-    }
-}
-//Represents a product, or a row in the Products table.
-module.exports.Product = class extends DBItem {
-    constructor(id, params) {
-        super(id, params, ['title', 'manufacturer', 'price', 'image', 'description']);
-        this.table = 'Products';
-    }
-    /*Get all the categories this product belongs to.
-    callback will be called with an array of category names (strings) as parameter*/
-    getCategories(callback) {
-        catobj = new ProdCategory();
-        catobj.selectMany('prodid', this.id, objs => callback(objs.map(x => x.props.category)));
-    }
-}
-//Represents a purchase, or a row in the Purchases table.
-module.exports.Purchase = class extends DBItem {
-    constructor(id, params) {
-        super(id, params, ['userid', 'prodid']);
-        this.table = 'Purchases';
-    }
-}
-//Represents a row in the Categories table.
-//Usually created like new ProdCategory() (without parameters) because individual rows don't have much value.
-module.exports.ProdCategory = class extends DBItem {
-    constructor(id, params) {
-        super(id, params, ['prodid', 'category']);
-        this.table = 'Categories';
-    }
-    /*Get all categories that exist. callback will be called with an array of category names (strings).*/    
-    getCategories(callback) {
-        db.all(`SELECT DISTINCT category FROM ${this.table};`, objs => callback(objs.map(x => x.category)));
-    }
-    /*Get all products that belong to the category given as the first argument.
-    callback will be called with an array of Products. sel is a boolean indicating if the properties of the
-    Products are important. If true, the select method will be called and all of the properties will be available.
-    Otherwise only the id will be available until you call select yourself.*/
-    getItems(category, callback, sel) {
-        var i = 0;
-        var items = [];
-        var length = 0;
-        /*Help function that increases i when a Product.select call is done.
-        Once all calls are done, calls the callback function.*/
-        function f() {
-            i++;
-            if (i == length)
-                callback(items);
+
+module.exports = {
+    //Represents a user, or a row in the Users table.
+    User: class extends DBItem {
+        constructor(id, params) {
+            super(id, params, ['login', 'password', 'first_name', 'last_name', 'email']);
+            this.table = 'Users'
         }
-        db.all(`SELECT prodid FROM ${this.table} WHERE category = ?;`, category, objs => {
-            length = objs.length;
-            for (var j = 0; j < length; j++) {
-                items[j] = new Product(objs[j].prodid);
-                if (sel) items[j].select(f);
+    },
+    //Represents a product, or a row in the Products table.
+    Product: class extends DBItem {
+        constructor(id, params) {
+            super(id, params, ['title', 'manufacturer', 'price', 'image', 'description']);
+            this.table = 'Products';
+        }
+        /*Get all the categories this product belongs to.
+        callback will be called with an array of category names (strings) as parameter*/
+        getCategories(callback) {
+            catobj = new ProdCategory();
+            catobj.selectMany('prodid', this.id, objs => callback(objs.map(x => x.props.category)));
+        }
+    },
+    //Represents a purchase, or a row in the Purchases table.
+    Purchase: class extends DBItem {
+        constructor(id, params) {
+            super(id, params, ['userid', 'prodid']);
+            this.table = 'Purchases';
+        }
+    },
+    //Represents a row in the Categories table.
+    //Usually created like new ProdCategory() (without parameters) because individual rows don't have much value.
+    ProdCategory: class extends DBItem {
+        constructor(id, params) {
+            super(id, params, ['prodid', 'category']);
+            this.table = 'Categories';
+        }
+        /*Get all categories that exist. callback will be called with an array of category names (strings).*/    
+        getCategories(callback) {
+            db.all(`SELECT DISTINCT category FROM ${this.table};`, objs => callback(objs.map(x => x.category)));
+        }
+        /*Get all products that belong to the category given as the first argument.
+        callback will be called with an array of Products. sel is a boolean indicating if the properties of the
+        Products are important. If true, the select method will be called and all of the properties will be available.
+        Otherwise only the id will be available until you call select yourself.*/
+        getItems(category, callback, sel) {
+            var i = 0;
+            var items = [];
+            var length = 0;
+            /*Help function that increases i when a Product.select call is done.
+            Once all calls are done, calls the callback function.*/
+            function f() {
+                i++;
+                if (i == length)
+                    callback(items);
             }
-            if (sel) f();
-            else callback(this.items); //no select calls to wait for, call callback directly
-        });
-    }
-}
-//Closes the database. Call this when you are done with all the queries you wanted to do.
-module.exports.close = db.close;
+            db.all(`SELECT prodid FROM ${this.table} WHERE category = ?;`, category, objs => {
+                length = objs.length;
+                for (var j = 0; j < length; j++) {
+                    items[j] = new Product(objs[j].prodid);
+                    if (sel) items[j].select(f);
+                }
+                if (sel) f();
+                else callback(this.items); //no select calls to wait for, call callback directly
+            });
+        }
+    },
+    //Closes the database. Call this when you are done with all the queries you wanted to do.
+    close: db.close
+};
